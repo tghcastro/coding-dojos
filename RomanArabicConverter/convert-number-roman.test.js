f255457bcef7a5781ee3372715d37c4d425f25ce
Thiago de Castro <tghcastro@gmail.com>
2022-04-05 16:32:19 +0100

    10 positions coverage

--- RomanArabicConverter/convert-number-roman.test.js
+++ RomanArabicConverter/convert-number-roman.test.js
@@ -2,9 +2,37 @@ const romanConverter = require('./romanConverter')
 const arabicoConverter = require('./arabicoConverter')
 const expect = require('expect')
 
+
+// I - 1 ± 2 - II § 3 - III ± 4 - IV ± 5 - V
+// VI - 6 ± 2 - VII § 7 - VIII ± 8 - IX ± 9 - X
+
 test('Convert 1 to I', () => {
     const valueToConvert = 1
     const expectedResult = "I"
+    const result = romanConverter.convertToRoman(valueToConvert)
+
+    expect(result).toBe(expectedResult);
+});
+
+test('Convert 2 to II', () => {
+    const valueToConvert = 2
+    const expectedResult = "II"
+    const result = romanConverter.convertToRoman(valueToConvert)
+
+    expect(result).toBe(expectedResult);
+});
+
+test('Convert 3 to III', () => {
+    const valueToConvert = 3
+    const expectedResult = "III"
+    const result = romanConverter.convertToRoman(valueToConvert)
+
+    expect(result).toBe(expectedResult);
+});
+
+test('Convert 4 to IV', () => {
+    const valueToConvert = 4
+    const expectedResult = "IV"
     const result = romanConverter.convertToRoman(valueToConvert)
 
     expect(result).toBe(expectedResult);
@@ -18,6 +46,38 @@ test('Convert 5 to V', () => {
     expect(result).toBe(expectedResult);
 });
 
+test('Convert 6 to VI', () => {
+    const valueToConvert = 6
+    const expectedResult = "VI"
+    const result = romanConverter.convertToRoman(valueToConvert)
+
+    expect(result).toBe(expectedResult);
+});
+
+test('Convert 7 to VII', () => {
+    const valueToConvert = 7
+    const expectedResult = "VII"
+    const result = romanConverter.convertToRoman(valueToConvert)
+
+    expect(result).toBe(expectedResult);
+});
+
+test('Convert 8 to VIII', () => {
+    const valueToConvert = 8
+    const expectedResult = "VIII"
+    const result = romanConverter.convertToRoman(valueToConvert)
+
+    expect(result).toBe(expectedResult);
+});
+
+test('Convert 9 to IX', () => {
+    const valueToConvert = 9
+    const expectedResult = "IX"
+    const result = romanConverter.convertToRoman(valueToConvert)
+
+    expect(result).toBe(expectedResult);
+});
+
 test('Convert 10 to X', () => {
     const valueToConvert = 10
     const expectedResult = "X"
@@ -26,13 +86,7 @@ test('Convert 10 to X', () => {
     expect(result).toBe(expectedResult);
 });
 
-test('Convert 2 to II', () => {
-    const valueToConvert = 2
-    const expectedResult = "II"
-    const result = romanConverter.convertToRoman(valueToConvert)
 
-    expect(result).toBe(expectedResult);
-});
 
 test('Convert I to 1', () => {
     const valueToConvert = "I"
--- RomanArabicConverter/romanConverter.js
+++ RomanArabicConverter/romanConverter.js
@@ -5,8 +5,29 @@ class RomanConverter {
         if (valueToConvert == 1) {
             valueToReturn = "I"
         }
+        if (valueToConvert == 2) {
+            valueToReturn = "II"
+        }
+        if (valueToConvert == 3) {
+            valueToReturn = "III"
+        }
+        if (valueToConvert == 4) {
+            valueToReturn = "IV"
+        }
         if (valueToConvert == 5) {
             valueToReturn = "V"
+        }
+        if (valueToConvert == 6) {
+            valueToReturn = "VI"
+        }
+        if (valueToConvert == 7) {
+            valueToReturn = "VII"
+        }
+        if (valueToConvert == 8) {
+            valueToReturn = "VIII"
+        }
+        if (valueToConvert == 9) {
+            valueToReturn = "IX"
         }
         if (valueToConvert == 10) {
             valueToReturn = "X"
